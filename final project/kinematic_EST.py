4feba29e8daa963c229ee4a25458b4136750d846
engdaniloifs <eng.danilosantosoliveira@gmail.com>
2025-11-17 16:47:16 -0500

    Update kinematic_EST.py

--- final project/kinematic_EST.py
+++ final project/kinematic_EST.py
@@ -13,30 +13,18 @@ def distance(node1, node2):
 def wrap_to_pi(a):
     return (a + np.pi) % (2*np.pi) - np.pi
 
-def sample_random_node(nodes,new_node,radius_density,iterations,w,flag_obstacle,last_max_w,w_line,nodes_test,sum_w_line):
+def sample_random_node(nodes,new_node,radius_density,iterations,w,flag_obstacle,last_max_w,w_line,nodes_test):
     iterations += 1
-    time_to_sample = time.time()
-    t_1_end = 0
-    t_2_end = 0
-    t_3_end = 0
-    t_4_end = 0
-    t_1_check_end = 0
     if flag_obstacle:
-        
-        t_1 = time.time()
         changed_w = []
         
         xy = nodes_test[:len(nodes)]
         t_1_check = time.time()
         kdt = cKDTree(xy)
-        t_1_check_end = time.time() - t_1_check
         # Find indices of nodes within radius_density
         idxs = kdt.query_ball_point([new_node[0], new_node[1]], r=radius_density)
-        
         # For each neighbor, check the heading difference
         last_max_w_changed = False
-        t_1_end = time.time() - t_1
-        t_2 = time.time()
         
         for i in idxs:
             node1 = nodes[i]
@@ -46,41 +34,17 @@ def sample_random_node(nodes,new_node,radius_density,iterations,w,flag_obstacle,
                     last_max_w_changed = True
                     last_max_w = w[i]
                 changed_w.append(i)
-        t_2_end = time.time() - t_2
-        t_3 = time.time()
         if last_max_w_changed:
-            sum_w_line = 0
-        
-            
             w_line = []
-            
             for i in range(len(nodes)):
-                w_line_value = last_max_w + 1 - w[i]       # assuming w is a list/array
+                w_line_value = last_max_w + 1 - w[i]       
                 w_line.append(w_line_value)
-                sum_w_line += w_line_value
-    
-            t_3_end = time.time() - t_3
         else:
-            t_4 = time.time()
             for i in changed_w:
-                old_wl = w_line[i]
-                new_wl = last_max_w + 1 - w[i]
-                w_line[i] = new_wl
-                sum_w_line += (new_wl - old_wl)  
-            
-            t_4_end = time.time() - t_4
-        if iterations > 1000 :
-            time_to_sample_end = time.time() - time_to_sample
-            
-        
-    t_5 = time.time()
+                w_line[i] = last_max_w + 1 - w[i]
+    
     selected_node = random.choices(nodes, weights=w_line, k=1)[0]
-    t_5_end = time.time() - t_5
-    time_to_sample_end = time.time() - time_to_sample
-    # if iterations >5000:
-    #      print(f"Time checking neighbors: {1000*t_1_end}, Time updating weights: {1000*t_2_end}, Time updating w_line full: {1000*t_3_end}, Time updating w_line partial: {1000*t_4_end}, Time selecting node: {1000*t_5_end}, Time to sample total: {1000*time_to_sample_end}, Time to check neighbors: {1000*t_1_check_end}")
-    #      input("Press Enter to continue...")
-    return selected_node,iterations,w,last_max_w,w_line,sum_w_line
+    return selected_node,iterations,w,last_max_w,w_line
 
 
 
@@ -91,9 +55,11 @@ def forward_propagate(state, control_input, step_time,car_track_length,obstacles
     L = car_track_length
     #print("Forward propagate from:",state,"with steering:",delta)
     #time.sleep(1)
-    little_step = step_time / 20
+    steps_number = 40
+    little_step = step_time / steps_number
+    
     time_elapsed = time.time()
-    for _ in range(10):
+    for _ in range(steps_number):
         x_new = x + v * np.cos(theta) * little_step
         y_new = y + v * np.sin(theta) * little_step
         theta_new = theta + (v / L) * np.tan(delta) * little_step
@@ -103,12 +69,12 @@ def forward_propagate(state, control_input, step_time,car_track_length,obstacles
             return (x_new, y_new, theta_new), False,False
         if not (-2 <= x_new <= 2 and -2 <= y_new <= 2):
             return (x_new, y_new, theta_new), False,False
-        if distance((x_new,y_new),goal) < 0.05 and abs(wrap_to_pi(theta_new - goal[2]))< np.deg2rad(20):
+        if distance((x_new,y_new),goal) < 0.05 and abs(wrap_to_pi(theta_new - goal[2]))< np.deg2rad(30):
+            input("Goal reached during forward propagate!")
             return (x_new, y_new, theta_new), True, True
         
-    time_elapsed = time.time() - time_elapsed
+        
     
-    #input("Time for forward propagate (s):"+str(time_elapsed))
     return (x, y, theta), True,False
 
 def is_in_obstacle(point,obstacles,car_size = None):
@@ -121,8 +87,8 @@ def is_in_obstacle(point,obstacles,car_size = None):
             width_car, length_car = car_size
             c = abs(np.cos(ptheta))
             s = abs(np.sin(ptheta))
-            obstacle_check_x = w/2 + (width_car/2)*c + (length_car/2)*s
-            obstacle_check_y = h/2 + (width_car/2)*s + (length_car/2)*c
+            obstacle_check_x = w/2 + (length_car/2)*c + (width_car/2)*s
+            obstacle_check_y = h/2 + (length_car/2)*s + (width_car/2)*c
         else:
             obstacle_check_x = w/2
             obstacle_check_y = h/2
@@ -132,14 +98,28 @@ def is_in_obstacle(point,obstacles,car_size = None):
 
 def build_EST(start,goal,X,obstacles,radius_density, car_size, step_time,L,trial = 0,plot = False):
     nodes = [start]
+    np.random.seed(0)
+    random.seed(0)
     if plot:
+        print("cs")
         x_start,y_start, theta_start = start 
         x_goal,y_goal,theta_goal = goal
-        plt.figure(figsize=(6, 6))
+        plt.figure(figsize=(10, 10))
         plt.plot(x_start,y_start, 'bo', markersize=7, label="Start")
         plt.quiver(x_start, y_start, 0.03*np.cos(theta_start), 0.03*np.sin(theta_start), angles='xy', scale_units='xy', scale=0.3, width=0.003)
         plt.plot(x_goal,y_goal, 'ro', markersize=7, label="Goal")
         plt.quiver(x_goal, y_goal, 0.03*np.cos(theta_goal), 0.03*np.sin(theta_goal), angles='xy', scale_units='xy', scale=0.3, width=0.003)
+        plt.gca().add_patch(plt.Circle((x_goal, y_goal), 0.05, color='red', alpha=0.15))
+
+# main heading arrow
+        plt.quiver(x_goal, y_goal, 0.03*np.cos(theta_goal), 0.03*np.sin(theta_goal),
+                angles='xy', scale_units='xy', scale=0.3, color='r', width=0.003)
+
+        # two limit arrows (± tolerance)
+        for sign in (-1, 1):
+            plt.quiver(x_goal, y_goal, 0.03*np.cos(theta_goal + sign*np.deg2rad(20)),
+                    0.03*np.sin(theta_goal + sign*np.deg2rad(20)),
+                    angles='xy', scale_units='xy', scale=0.3, color='r', alpha=0.6, width=0.003)
         plt.xlim(X[0])
         plt.ylim(X[1])
         plt.title("EST in progress")
@@ -154,71 +134,32 @@ def build_EST(start,goal,X,obstacles,radius_density, car_size, step_time,L,trial
             
             ax.add_patch(rect)
 
-        #plt.show()
         nodes = [start]
-        nodes_test = np.zeros((20000,2))
+        nodes_test = np.zeros((30000,2))
         nodes_test[0] = np.array([start[0],start[1]])
         edges = []
         
         solution_length = {start: 0}
 
         iterations = 0
-        #plt.pause(0.1)
         x_new = start
         w = [0]
-        discrete_steerings = [-0.6, 0, 0.6]
+        discrete_steerings = (-0.6,0.6)
         free_of_obstacles = True
-        
-        p = {}
         last_max_w = 0
         w_line = {}
         t_inicial = time.time()
-        sum_w_line = 0
-        reset = True
-        checkpoint = 1000
-        sample_handcap = 0
-        sample_count = 0
         flag_goal = False
-        while not flag_goal:
-            # if reset:
-            #     t_inicial = time.time()
-            #     reset = False
-            t_1 = time.time()
-            if iterations % 10000 == 0 and iterations > 0:
-                t_elapsed = time.time() - t_inicial
-                print("Iterations:",iterations)
-                print("Elapsed time (s):", t_elapsed) 
-                plt.pause(10)
-                 #reset = True
-            #if iterations % 2000 == 0 and iterations > 0:
-             #   print("Iterations:",iterations)
-              #  t_elapsed = time.time() - t_inicial
-               # print("Elapsed time (s):", t_elapsed) 
-                #input("Press Enter to continue...")
-
-            # if iterations >= checkpoint:
-            #     checkpoint += 1000
-            #     sample_handcap += 10
-            #     sample_count = 0
-            # if free_of_obstacles:
-            #     if sample_count < sample_handcap:
-            #         free_of_obstacles = False
-            #         sample_count = 0
-            #     else:
-            #         free_of_obstacles = True
-            v_src,iterations,w,last_max_w,w_line,sum_w_line = sample_random_node(nodes,x_new,
-                                                                                   radius_density,iterations,w,
+       
+        while not flag_goal: #and time.time() - t_inicial < 30:
+            
+                
+           
+            v_src,iterations,w,last_max_w,w_line = sample_random_node(nodes,x_new, radius_density,iterations,w,
                                                                                    free_of_obstacles,
-                                                                                   last_max_w,w_line,nodes_test,sum_w_line)
-            sample_count +=1
-            t_1_end = time.time() - t_1
-            t_2 = time.time()
-            control_input = np.random.choice(discrete_steerings)
-            t_2_end = time.time() - t_2
-            t_3 = time.time()
+                                                                                   last_max_w,w_line,nodes_test)
+            control_input = np.random.uniform(*discrete_steerings)
             x_new,free_of_obstacles,flag_goal = forward_propagate(v_src, control_input, step_time,L,obstacles,car_size,goal)
-            t_3_end = time.time() - t_3
-            t_4 = time.time()
             if free_of_obstacles:
                 nodes.append(x_new)
                 nodes_test[len(nodes)-1] = [x_new[0],x_new[1]]
@@ -232,39 +173,37 @@ def build_EST(start,goal,X,obstacles,radius_density, car_size, step_time,L,trial
                     plt.plot(x_new[0], x_new[1], 'go', markersize=2)  
                     plt.quiver(x_new[0], x_new[1], 0.03*np.cos(x_new[2]),
                                 0.03*np.sin(x_new[2]), angles='xy', scale_units='xy', scale=0.3, width=0.003)
-                    #plt.pause(0.1)
-            t_4_end = time.time() - t_4
-            t_loop_end = time.time() - t_1
-            # if iterations >5000:
-            #      print(f"Time sampling: {1000*t_1_end}, Time control input: {1000*t_2_end}, Time forward propagate: {1000*t_3_end}, Time adding node: {1000*t_4_end}")
-            #      print("Total loop time:", 1000*t_loop_end)
-            #      input("Press Enter to continue...")
-        node = x_new
-        path = []
-        while node != start:
-            edge = None
-            for (n1, n2) in edges:
-                if n2 == node:
-                    edge = (n1, n2)
-                    break
-            if edge in edges:
-                path.append(edge)
-            node = n1
-        path_demo = []
-        for (p, c) in reversed(path):   # go from start → goal
-            x1, y1,theta1 = p
-            x2, y2,theta2 = c
-            path_demo.append((x1,y1,theta1))
-            plt.plot([x1, x2], [y1, y2], 'r-', linewidth=1)  
-        path_demo.append(goal)
-        plt.pause(1)
-        plt.title(f"EST Finished - Trial number {trial+1}")
-        plt.show()
-        print("Iterations:",iterations)
-        print("Vertices:",len(nodes))
-        print("Solution length:", solution_length[goal])
+        if flag_goal:
+            node = x_new
+            path = []
+            while node != start:
+                edge = None
+                for (n1, n2) in edges:
+                    if n2 == node:
+                        edge = (n1, n2)
+                        break
+                if edge in edges:
+                    path.append(edge)
+                node = n1
+            path_demo = []
+            for (p, c) in reversed(path):   # go from start → goal
+                x1, y1,theta1 = p
+                x2, y2,theta2 = c
+                path_demo.append((x1,y1,theta1))
+                plt.plot([x1, x2], [y1, y2], 'r-', linewidth=3)  
+            path_demo.append(goal)
+            plt.title(f"EST Finished - iterations {iterations}")
+            plt.pause(1)
+            
+            plt.show()
+            print("Iterations:",iterations)
+            print("Vertices:",len(nodes))
+            # print("Solution length:", solution_length[goal])
         
-        return iterations, len(nodes), solution_length[goal], path_demo
+            return iterations, len(nodes), iterations
+        else:
+            print("No solution found in time limit.")
+            return iterations, len(nodes), float('inf')
                     
 
 
@@ -272,11 +211,10 @@ def build_EST(start,goal,X,obstacles,radius_density, car_size, step_time,L,trial
 def main():
 
     xlim,ylim,thetalim = (-2, 2),(-2,2), (-np.pi, np.pi)
-    start = (-1.5, 0, np.deg2rad(0))
-    goal = (0.75, 0, np.deg2rad(0))
+    start = (-1.2, 0, np.deg2rad(0))
+    goal = (1.2, 0, np.deg2rad(0))
     control_limits = [(0,1), (-0.5,0,0.5)] # speed, steering angle
-    
-    step_size = 0.5
+    step_size = 0.4
     radius_density = step_size
     speed = 0.1
     step_time = step_size / speed
@@ -298,8 +236,14 @@ def main():
     car_size = [width, length]
 
 
-    iterations, vertices, solution_length, = build_EST(start,goal,X,obstacles,radius_density,
-                                                        car_size,step_time, L,trial = 0,plot = True)
+   
+    for trial in range(trials_number):
+        print("Trial number:",trial+1)
+        iterations, vertices, solution_length = build_EST(start,goal,X,obstacles,radius_density,
+                                                            car_size,step_time, L,trial,plot = True)
+        print("Trial",trial+1,"completed.")
+        plt.show()
+        
 
 if __name__ == "__main__":
     main()
